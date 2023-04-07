dfbaa13378863f546e74a26d8d2115d71559f867
duswnsxnxn <duswnsxnxn@gmail.com>
2023-04-07 21:25:58 +0900

    refactor: LikeablePerson엔티티 수정

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -30,7 +30,7 @@ public class LikeablePerson {
     @ManyToOne
     private InstaMember fromInstaMember; // 호감을 표시한 사람(인스타 멤버)
     private String fromInstaMemberUsername; // 혹시 몰라서 기록
-    @ManyToOne // 호감인 사람을 호감리스트에서 삭제 시 연관된 InstaMember 엔티티도 함께 삭제하기 위해 영속성 전이 속성을 주었다.
+    @ManyToOne(cascade = CascadeType.REMOVE) // 호감인 사람을 호감리스트에서 삭제 시 연관된 InstaMember 엔티티도 함께 삭제하기 위해 영속성 전이 속성을 주었다.
     private InstaMember toInstaMember; // 호감을 받은 사람(인스타 멤버)
     private String toInstaMemberUsername; // 혹시 몰라서 기록
     private int attractiveTypeCode; // 매력포인트(1=외모, 2=성격, 3=능력)
