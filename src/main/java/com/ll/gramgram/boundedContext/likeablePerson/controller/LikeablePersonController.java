fc2c344cc942e13f7f9cb979561e0f58fa3a843d
duswnsxnxn <duswnsxnxn@gmail.com>
2023-04-10 14:46:22 +0900

    config: 깃 초기화

---
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -0,0 +1,82 @@
+package com.ll.gramgram.boundedContext.likeablePerson.controller;
+
+import com.ll.gramgram.base.rq.Rq;
+import com.ll.gramgram.base.rsData.RsData;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/likeablePerson")
+@RequiredArgsConstructor
+public class LikeablePersonController {
+    private final Rq rq;
+    private final LikeablePersonService likeablePersonService;
+
+    @GetMapping("/add")
+    public String showAdd() {
+        return "usr/likeablePerson/add";
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class AddForm {
+        private final String username;
+        private final int attractiveTypeCode;
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid AddForm addForm) {
+        RsData<LikeablePerson> createRsData = likeablePersonService.like(rq.getMember(), addForm.getUsername(), addForm.getAttractiveTypeCode());
+
+        if (createRsData.isFail()) {
+            return rq.historyBack(createRsData);
+        }
+
+        return rq.redirectWithMsg("/likeablePerson/list", createRsData);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/list")
+    public String showList(Model model) {
+        InstaMember instaMember = rq.getMember().getInstaMember();
+
+        // 인스타인증을 했는지 체크
+        if (instaMember != null) {
+            List<LikeablePerson> likeablePeople = likeablePersonService.findByFromInstaMemberId(instaMember.getId());
+            model.addAttribute("likeablePeople", likeablePeople);
+        }
+
+        return "usr/likeablePerson/list";
+    }
+
+    @PreAuthorize("isAuthenticated()") // 인증처리
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        LikeablePerson person = likeablePersonService.findById(id).orElse(null);
+        RsData<LikeablePerson> checked = likeablePersonService.checkAuthorize(rq.getMember(), person);
+        if (checked.isFail()) {
+            return rq.redirectWithMsg("/likeablePerson/list", checked);
+        }
+
+        RsData<LikeablePerson> deleted = likeablePersonService.deleteLikeablePerson(person);
+        if (deleted.isFail()) {
+            return rq.redirectWithMsg("/likeablePerson/list", deleted);
+        }
+        return rq.redirectWithMsg("/likeablePerson/list", deleted);
+    }
+
+}
---
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
@@ -0,0 +1,82 @@
+package com.ll.gramgram.boundedContext.likeablePerson.service;
+
+import com.ll.gramgram.base.rsData.RsData;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.likeablePerson.repository.LikeablePersonRepository;
+import com.ll.gramgram.boundedContext.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class LikeablePersonService {
+    private final LikeablePersonRepository likeablePersonRepository;
+    private final InstaMemberService instaMemberService;
+
+    @Transactional
+    public RsData<LikeablePerson> like(Member member, String username, int attractiveTypeCode) {
+        if (member.hasConnectedInstaMember() == false) {
+            return RsData.of("F-2", "먼저 본인의 인스타그램 아이디를 입력해야 합니다.");
+        }
+
+        if (member.getInstaMember().getUsername().equals(username)) {
+            return RsData.of("F-1", "본인을 호감상대로 등록할 수 없습니다.");
+        }
+
+        InstaMember toInstaMember = instaMemberService.findByUsernameOrCreate(username).getData();
+
+        LikeablePerson likeablePerson = LikeablePerson
+                .builder()
+                .fromInstaMember(member.getInstaMember()) // 호감을 표시하는 사람의 인스타 멤버
+                .fromInstaMemberUsername(member.getInstaMember().getUsername()) // 중요하지 않음
+                .toInstaMember(toInstaMember) // 호감을 받는 사람의 인스타 멤버
+                .toInstaMemberUsername(toInstaMember.getUsername()) // 중요하지 않음
+                .attractiveTypeCode(attractiveTypeCode) // 1=외모, 2=능력, 3=성격
+                .build();
+
+        likeablePersonRepository.save(likeablePerson); // 저장
+
+        return RsData.of("S-1", "입력하신 인스타유저(%s)를 호감상대로 등록되었습니다.".formatted(username), likeablePerson);
+    }
+
+    public List<LikeablePerson> findByFromInstaMemberId(Long fromInstaMemberId) {
+        return likeablePersonRepository.findByFromInstaMemberId(fromInstaMemberId);
+    }
+
+    public Optional<LikeablePerson> findById(Long id) {
+        return likeablePersonRepository.findById(id);
+    }
+
+    public RsData<LikeablePerson> checkAuthorize(Member member, LikeablePerson likeablePerson) {
+
+        if (!member.hasConnectedInstaMember()) {
+            return RsData.of("F-2", "먼저 본인의 인스타그램 아이디를 입력해야 합니다.");
+        }
+        if (likeablePerson == null) {
+            return RsData.of("F-2", "입력하신 인스타유저는 호감리스트에 존재하지 않습니다.");
+        }
+
+        long actorInstaMeberId = member.getInstaMember().getId();
+        long likeableFromInstaMember = likeablePerson.getFromInstaMember().getId();
+        if (actorInstaMeberId != likeableFromInstaMember) {
+            return RsData.of("F-2", "권한이 없습니다.");
+        }
+        return RsData.of("S-1", "권한이 있습니다.");
+    }
+
+    @Transactional
+    public RsData<LikeablePerson> deleteLikeablePerson(LikeablePerson likeablePerson) {
+
+        String name = likeablePerson.getToInstaMemberUsername();
+        likeablePersonRepository.delete(likeablePerson);
+        return RsData.of("S-1", "입력하신 인스타유저(%s)를 호감리스트에서 삭제했습니다..".formatted(name));
+    }
+
+}
